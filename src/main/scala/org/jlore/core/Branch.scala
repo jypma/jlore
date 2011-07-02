0f238bcc0cea25dd2042df64f357c39e738de583
Jan Ypma <github@jan.ypmania.nl>
2011-07-02 13:38:00 +0200

    branch sorting now working

--- src/main/scala/org/jlore/core/Branch.scala
+++ src/main/scala/org/jlore/core/Branch.scala
@@ -1,16 +1,27 @@
 package org.jlore.core
-import scala.collection.immutable.TreeSet
+import scala.collection.immutable.TreeMap
 
 class Branch (
   val command: Command = new Branch.Create(ID()),
-  val tail: TreeSet[Branch] = TreeSet.empty,
+  val tail: TreeMap[Command,Branch] = TreeMap.empty,
   val latestVersions: Map[VersionedObject[Any],ObjectVersion[Any]] = Map.empty
 ) extends Ordered[Branch] {
   def compare (other:Branch) = command.compare (other.command)
-  def + (c: Command) = {
-    val newObjs = c.run(this)
-    val newVersions = latestVersions ++ (newObjs map { v => (v.obj -> v) })
-    new Branch (c, tail + this, newVersions)
+  def + (c: Command): Branch = {
+    if (c > command) {
+      val newObjs = c.run(this)
+      val newVersions = latestVersions ++ (newObjs map { v => (v.obj -> v) })
+      val newTail = tail + (command -> this)
+      new Branch (c, newTail, newVersions)
+    } else {
+      var branch = tail.to (c).last._2
+      // create new branches for rest of tail
+      tail.from(c).keys.foreach { cmd =>
+        branch += cmd
+      }
+      // create new branch for head and return that
+      branch + command
+    }
   }
   def get [T] (obj: VersionedObject[Any]): T = {
     latestVersions(obj).asInstanceOf[T]
